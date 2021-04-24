fa244f8f185b7829c3b5fc7135d6634558e3040f
ds17 <ingsed@outlook.com>
2021-04-24 11:35:59 +0800

    update

--- C++/initialize.cpp
+++ C++/initialize.cpp
@@ -1,6 +1,6 @@
-//4中初始化并赋值的方式
+//4种初始化并赋值的方式
 
-int units_sold = 0;
-int units_sold = {0};
-int units_sold(0);
-int units_sold{0}；
+int units_sold = 0;   //拷贝初始化
+int units_sold = {0}; //列表初始化
+int units_sold(0);   //直接初始化
+int units_sold{0};
